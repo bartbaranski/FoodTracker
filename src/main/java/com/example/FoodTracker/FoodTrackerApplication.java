69ba6d029c7a2f549417a3f3fdef82727a22cd38
Bartłomiej Barański <147482483+bartbaranski@users.noreply.github.com>
2024-11-10 00:23:11 +0100

    Add files via upload

---
+++ src/main/java/com/example/FoodTracker/FoodTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTrackerApplication.class, args);
+	}
+
+}
